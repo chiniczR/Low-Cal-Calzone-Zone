884dfeefd2f1e8a4447a6befed736a8dd50b85f3
Rebeca Chinicz <chiniczrebeca@gmail.com>
2020-11-15 15:58:59 +0200

    Adds kitchen column

--- src/App.js
+++ src/App.js
@@ -1,82 +1,192 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import Menu from './components/Menu/Menu';
-import OrderPad from './components/OrderPad/OrderPad';
+import React, { useState } from "react";
+import "./App.css";
+import { Container, Row, Col, Button, Modal, Form } from "react-bootstrap";
+import Menu from "./components/Menu/Menu";
+import OrderPad from "./components/OrderPad/OrderPad";
+import Kitchen from "./components/Kitchen/Kitchen";
+import OrderStack from "./components/OrderStack/OrderStack";
 
 function App() {
-  const [food, setFood] = useState([])
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [food, setFood] = useState([]);
   const pushFood = (order) => {
     let f = [...food];
-    let existInd = f.findIndex(o => o.size === order.size && order.fillings.every(fil => o.fillings.includes(fil)));
+    let existInd = f.findIndex(
+      (o) =>
+        o.size === order.size &&
+        order.fillings.every((fil) => o.fillings.includes(fil))
+    );
     if (existInd > -1) {
       f[existInd].quantity += 1;
-    }
-    else {
+    } else {
       f.push({ size: order.size, fillings: order.fillings, quantity: 1 });
     }
     setFood(f);
-  }
-  const dropFood = (order, all=false) => {
-    console.log('Got to dropFood:  order =', JSON.stringify(order), 'all =', all)
+  };
+  const dropFood = (order, all = false) => {
+    console.log(
+      "Got to dropFood:  order =",
+      JSON.stringify(order),
+      "all =",
+      all
+    );
     let f = [...food];
-    let existInd = f.findIndex(o => o.size === order.size && order.fillings.every(fil => o.fillings.includes(fil)));
+    let existInd = f.findIndex(
+      (o) =>
+        o.size === order.size &&
+        order.fillings.every((fil) => o.fillings.includes(fil))
+    );
     if (existInd > -1) {
       if (all) {
-        f = f.filter(o => o.size !== order.size && !order.fillings.every(fil => o.fillings.includes(fil)))
-      }
-      else {
+        f = f.filter(
+          (o) =>
+            o.size !== order.size &&
+            !order.fillings.every((fil) => o.fillings.includes(fil))
+        );
+      } else {
         f[existInd].quantity -= 1;
       }
     }
     setFood(f);
-  }
+  };
 
-  const [drinks, setDrinks] = useState([])
+  const [drinks, setDrinks] = useState([]);
   const pushDrink = (order) => {
     let d = [...drinks];
-    let existInd = d.findIndex(o => o.drink === order);
+    let existInd = d.findIndex((o) => o.drink === order);
     if (existInd > -1) {
       d[existInd].quantity += 1;
-    }
-    else {
+    } else {
       d.push({ drink: order, quantity: 1 });
     }
     setDrinks(d);
-  }
-  const dropDrinks = (order, all=false) => {
+  };
+  const dropDrinks = (order, all = false) => {
     let d = [...drinks];
-    let existInd = d.findIndex(o => o.drink === order);
+    let existInd = d.findIndex((o) => o.drink === order);
     if (existInd > -1) {
       if (all) {
-        d = d.filter(o => o.drink !== order)
-      }
-      else {
+        d = d.filter((o) => o.drink !== order);
+      } else {
         d[existInd].quantity -= 1;
       }
     }
     setDrinks(d);
+  };
+  
+  const [orders, setOrders] = useState([]);
+  const [email, setEmail] = useState('');
+  const [addr, setAddr] = useState('');
+  const [stage, setStage] = useState('empty');
+
+  const handleSubmitOrder = (event) => {
+    event.preventDefault()
+    let newOrder = {
+      email: email,
+      addr: addr,
+      food: food,
+      drinks: drinks
+    };
+    /*
+     TODO: async save to database
+    */
+    let o = [...orders];
+    o.push(newOrder);
+    setOrders(o);
+    setShow(false);
+    setEmail('');
+    setAddr('');
+    setFood([]);
+    setDrinks([]);
+
+    setStage('baking'); // <-- Should only happen when we get a notification from the topic
   }
 
   return (
     <div className="App">
       <header className="App-header container-flex">
         <div className="row align-items-center">
-          <h1 className="text-outline"><b>The Low-Cal Calzone Zone</b></h1> 
+          <h1 className="text-outline">
+            <b>The Low-Cal Calzone Zone</b>
+          </h1>
         </div>
       </header>
       <Container fluid>
-          <Row>
-            <Col className="border-right">
-              <Menu orderFood={order => pushFood(order)} orderDrink={order => pushDrink(order)} />
-              <OrderPad className="mt-3" food={food} drinks={drinks} removeFood={dropFood} removeDrink={dropDrinks} />
-            </Col>
-            <Col className="border-right">
-            </Col>
-            <Col>
-            </Col>
-          </Row>
-        </Container>
+        <Row className="justify-content-center align-items-center">
+          <Col className="border-right">
+            <Menu
+              orderFood={(order) => pushFood(order)}
+              orderDrink={(order) => pushDrink(order)}
+            />
+            <OrderPad
+              className="mt-3 mb-0"
+              food={food}
+              drinks={drinks}
+              removeFood={dropFood}
+              removeDrink={dropDrinks}
+            />
+            <Button
+              disabled={food.length < 1 && drinks.length < 1}
+              title={
+                food.length < 1 && drinks.length < 1
+                  ? "Please select some food or drink first"
+                  : "Click here to place your order as defined above"
+              }
+              variant="outline-default"
+              className="my-0 bg-off-white border rounded-pill"
+              onClick={handleShow}
+            >
+              Place Order
+            </Button>
+          </Col>
+          <Col className="border-right">
+            <OrderStack orders={orders} />
+            <Kitchen stage={stage} />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+      <Modal show={show} onHide={handleClose} backdrop={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delivery Information</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={handleSubmitOrder}>
+          <Modal.Body>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Your email address</Form.Label>
+              <Form.Control 
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              />
+              <Form.Text className="text-muted">
+                If you are a frequent customer, your order will be delivered more quickly!
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicAddress">
+              <Form.Label>Enter the address you want your ordered delivered to</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your address"
+                value={addr}
+                onChange={(e) => setAddr(e.currentTarget.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Order
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
     </div>
   );
 }
---
+++ src/components/OrderStack/OrderStack.js
@@ -0,0 +1,53 @@
+import OrderCheck from "../../assets/images/order-check.jpg"
+import { Popover, Container, Row, OverlayTrigger } from "react-bootstrap";
+import "./OrderStack.css";
+
+function OrderStack ({orders}) {
+    const popover = (order, i) => {
+        return (
+            <Popover id="popover-basic">
+            <Popover.Title as="h3">Order No. {i + 1}</Popover.Title>
+            <Popover.Content>
+                Calzones:
+                <ul>
+                    {order.food.map((food, j) => {
+                        return (
+                            <li key={'food' + j}>{food.quantity}x {food.size} {food.fillings.length > 0 ? 'with ' + food.fillings.join(', ') : 'plain'}</li>
+                        );
+                    })}
+                </ul>
+                Drinks:
+                <ul>
+                    {order.drinks.map((drink, j) => {
+                        return (
+                            <li key={'drink' + j}>{drink.quantity}x {drink.drink}</li>
+                        );
+                    })}
+                </ul>
+            </Popover.Content>
+            </Popover>
+        )
+    };
+
+    let color = (i) => i === 0 ? 'primary' : 'dark';
+
+    let ordersToStack = orders.map((order, i) => {
+        return (
+            <span key={i} className="m-1">
+                <OverlayTrigger trigger="click" placement="right" overlay={popover(order, i)}>
+                    <img src={OrderCheck} alt="Order check" className={`btn-img border border-${color(i)} rounded`} height="100" />
+                </OverlayTrigger>
+            </span>
+        );
+    });
+
+    return (
+        <Container fluid className="my-2 mx-1">
+            <Row className="justify-content-center">
+                {ordersToStack}
+            </Row>
+        </Container>
+    );
+};
+
+export default OrderStack;
